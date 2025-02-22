20c587f3812dfa862f53b26a83e8e86bb4c747c0
michael r <michaelroxas.od@gmail.com>
2025-02-22 18:09:19 +0800

    Add files via work laptop
    Used zip for downloading and editing files.

--- script.js
+++ script.js
@@ -4,40 +4,57 @@ function generateGreeting() {
 
   const outputTextarea = document.getElementById("outputTextarea");
   outputTextarea.value = ""; // Clear the textarea
-
+  
+  const intro 				= `Good day, ${name}! Thank you for choosing OWNDAYS!`
+  
+  const pickUpMessage 		= `We're glad to inform you that your eyeglasses are now ready for pick up here at OWNDAYS Robinsons Antipolo.`
+  
+  const frameToFollowMessage= `We're glad to inform you that your lenses have been delivered to our store here at OWNDAYS Robinsons Antipolo.` 
+  
+  const frameToFollowReqs 	= `Kindly bring your frame and warranty card for assembly.`
+  
+  const confirmReceipt 		= `Please confirm when you have received this message.`
+  
+  const storeHours 			= `Store hours:\n10:00 AM - 10:00 PM\nMon - Sun`
+  
+  const storeHoursFrameToFollow = `Store hours:\n10:00 AM - 9:00 PM\nMon - Sun`
+  
+  const lineBrk  			= `\n`
+  
+  const unclaimedMsg 		= `We hope this message finds you well. We appreciate your visit to OWNDAYS Robinsons Antipolo. However, it has come to our attention that there are unclaimed items purchased under your name: `
+  
+  const ensureMsg			= `To ensure smooth and efficient process, we kindly request you to collect your purhcased items at your earliest convenience. Our team is ready to provide further assistance for any concerns you may have in collecting your said items.` 
+  
+  const unclaimedReminders  = `Please be reminded on our storage period below: \n\n1) Purchased items will be held by respective OWNDAYS branch for a maxium of 60 days from DATE OF PICK-UP/PURCHASE;\n\n2) After 60 days, purhcased items will be sent to our Head Offie c/o Operations for safekeeping for another 30 days.\n\n`
+  
+  const unclaimedDisregardMsg 		= `If you have already collected your items, please disregard this message. Thank you.` 
+  
+  const boilerPlateItems = `<qty, item/s>` 
+  
   if (name) {
     let message = "";
 
     if (greeting === "for-pick-up") {
-      message = `Good day, ${name}!\n` + `\n` + 
-                `Thank you for choosing OWNDAYS!\n` + `\n` + 
-                `We're glad to inform you that your eyeglasses are ready for pick up here at OWNDAYS Robinsons Antipolo.\n` + `\n` + 
-                `Please confirm when you have received this message.\n` + `\n` + 
-                `Thank you.\n` + `\n` + 
-                `Store hours:\n` +
-                `10:00 AM - 10:00 PM\n` +
-                `Mon - Sun`;
+      message = intro 					+ lineBrk + lineBrk + 
+				pickUpMessage 			+ lineBrk + lineBrk + 
+				confirmReceipt 			+ lineBrk + lineBrk + 
+				storeHours
+	  
     } else if (greeting === "frame-to-follow") {
-      message = `Good day, ${name}!\n` + `\n` + 
-                `Thank you for choosing OWNDAYS!\n` + `\n` + 
-                `We're glad to inform you that your lenses have been delivered to our store here at OWNDAYS Robinsons Antipolo.\n` +  `\n` + 
-                `Kindly bring your frame of choice and receipt for assembly.\n` + `\n` + 
-                `Please confirm when you have received this message.\n` + `\n` + 
-                `Thank you.\n` + `\n` + 
-                `Store Hours:\n` +
-                `10:00 AM - 10:00 PM\n` +
-                `Mon - Sun`;
+      message = intro 					+ lineBrk + lineBrk + 
+				frameToFollowMessage 	+ lineBrk + lineBrk + 
+				frameToFollowReqs 		+ lineBrk + lineBrk + 
+				confirmReceipt 			+ lineBrk + lineBrk + 
+				storeHoursFrameToFollow
+	  
+	  
     }else if (greeting === "unclaimed") {
-      message = `Good day, ${name}!\n` + `\n` + 
-                `We hope this message finds you well. We appreciate your visit to OWNDAYS Robinsons Antipolo. However, it has come to our attention that there are unclaimed items purchased under your name.\n` + `\n` + 
-                `To ensure smooth and efficient process, we kindly request you to collect your purhcased items at your earliest convenience. Our team is ready to provide further assistance for any concerns you may have in collecting your said items. \n` +  `\n` + 
-                `Please be reminded on our storage period below: \n` + `\n` + 
-                `1) Purchased items will be held by respective OWNDAYS branch for a maxium of 60 days from DATE OF PICK-UP/PURCHASE;\n` + 
-								`2) After 60 days, purhcased items will be sent to our Head Offie c/o Operations for safekeeping for another 30 days.\n ` + `\n` + 
-                `If you have already collected your items, please disregard this message. Thank you. \n` + `\n` + 
-                `Store Hours:\n` +
-                `10:00 AM - 10:00 PM\n` +
-                `Mon - Sun`;
+      message = intro 					+ lineBrk + lineBrk + 
+				unclaimedMsg 			+ lineBrk + lineBrk + 
+				boilerPlateItems 		+ lineBrk + lineBrk + 
+				unclaimedReminders + 
+				unclaimedDisregardMsg 	+ lineBrk + lineBrk + 
+				storeHours
     }
 
     outputTextarea.value = message;
