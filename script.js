42f2c3f75228b848687ff1e1cfc2a1452752d30b
michael r <michaelroxas.od@gmail.com>
2025-04-19 16:44:42 +0800

    Add date input and messages

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 function generateGreeting() {
+  
   const name = document.getElementById("nameInput").value;
+  const date = document.getElementById("dateInput").value;  
   const greetingRadio = document.querySelector('input[name="greeting"]:checked');
   const outputTextarea = document.getElementById("outputTextarea");
   outputTextarea.value = ""; // Clear the textarea
 
   const intro = `Good day, ${name}! Thank you for choosing OWNDAYS!`;
-  const introDelay = `Good day, ${name}!`;
+  
+  const introDelay = `Good day from OWNDAYS Robinsons Place Antipolo, ${name}!`;
+  
   const pickUpMessage = `We're glad to inform you that your eyeglasses are now ready for pick up here at OWNDAYS Robinsons Antipolo.`;
+  
   const frameToFollowMessage = `We're glad to inform you that your lenses have been delivered to our store here at OWNDAYS Robinsons Antipolo.`;
+  
+  const delayedLensDeliveryMessage = `Please be informed that there may be a delay in the delivery of your purchased lenses to our store.`; 
+  
+  const tentativeDate = `New tentative delivery date is: ${date}`;
+  
+  const contactUs = `For any other concerns, you may reach us through this number. Thank you for your patience and understanding.`; 
+  
   const frameToFollowReqs = `Kindly bring your frame and warranty card for assembly.`;
+  
   const confirmReceipt = `Please confirm when you have received this message.`;
+  
   const storeHours = `Store hours:\n10:00 AM - 10:00 PM\nMon - Sun`;
+  
   const storeHoursFrameToFollow = `Store hours:\n10:00 AM - 9:00 PM\nMon - Sun`;
+  
   const lineBrk = `\n`;
+  
   const unclaimedMsg = `We hope this message finds you well. We appreciate your visit to OWNDAYS Robinsons Antipolo. However, it has come to our attention that there are unclaimed items purchased under your name: `;
+  
   const ensureMsg = `To ensure smooth and efficient process, we kindly request you to collect your purchased items at your earliest convenience. Our team is ready to provide further assistance for any concerns you may have in collecting your said items.`;
+  
   const unclaimedReminders = `Please be reminded on our storage period below: \n\n1) Purchased items will be held by respective OWNDAYS branch for a maximum of 60 days from DATE OF PICK-UP/PURCHASE;\n\n2) After 60 days, purchased items will be sent to our Head Office c/o Operations for safekeeping for another 30 days.\n\n`;
+  
   const unclaimedDisregardMsg = `If you have already collected your items, please disregard this message. Thank you.`;
+  
   const boilerPlateItems = `<qty, item/s>`;
+  
   const plannerDelayMsg = `We're glad to inform you that your OWNDAYS Planner is now ready for pick up here at OWNDAYS Robinsons Place Antipolo.`;
+  
   const sayGratitudeMsg = `Thank you for your patience and kind understanding for the delay in receiving your item.`;
 
   if (name && greetingRadio) {
@@ -27,12 +50,19 @@ function generateGreeting() {
 
     if (greeting === "for-pick-up") {
       message = `${intro}${lineBrk}${lineBrk}${pickUpMessage}${lineBrk}${lineBrk}${confirmReceipt}${lineBrk}${lineBrk}${storeHours}`;
-    } else if (greeting === "frame-to-follow") {
+    
+	} else if (greeting === "frame-to-follow") {
       message = `${intro}${lineBrk}${lineBrk}${frameToFollowMessage}${lineBrk}${lineBrk}${frameToFollowReqs}${lineBrk}${lineBrk}${confirmReceipt}${lineBrk}${lineBrk}${storeHoursFrameToFollow}`;
-    } else if (greeting === "unclaimed") {
+
+	} else if (greeting === "delayed-lens-delivery") {
+	message = `${introDelay}${lineBrk}${lineBrk}${delayedLensDeliveryMessage}	${lineBrk}${lineBrk}${tentativeDate}${lineBrk}${lineBrk}${confirmReceipt}${lineBrk}${lineBrk}${contactUs}${lineBrk}${lineBrk}${storeHours}`;
+
+   } else if (greeting === "unclaimed") {
       message = `${intro}${lineBrk}${lineBrk}${unclaimedMsg}${lineBrk}${lineBrk}${boilerPlateItems}${lineBrk}${lineBrk}${unclaimedReminders}${unclaimedDisregardMsg}${lineBrk}${lineBrk}${storeHours}`;
+
     } else if (greeting === "delayed-planner") {
       message = `${introDelay}${lineBrk}${lineBrk}${sayGratitudeMsg}${lineBrk}${lineBrk}${plannerDelayMsg}${lineBrk}${lineBrk}${confirmReceipt}	${lineBrk}${lineBrk}${storeHours}`;
+
     }
 
     outputTextarea.value = message;
